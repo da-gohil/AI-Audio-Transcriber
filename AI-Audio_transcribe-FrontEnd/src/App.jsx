69ff2437b48dabb7bb011ef6f95a08079402c564
Darshan Gohil <157419078+da-gohil@users.noreply.github.com>
2025-11-04 20:02:52 -0500

    Create App.jsx

---
+++ AI-Audio_transcribe-FrontEnd/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import AudioUploader from './AudioUploader';
+
+
+
+function App() {
+  return (
+    <div className="app-wrapper">
+      <AudioUploader />
+    </div>
+  );
+}
+
+export default App;
